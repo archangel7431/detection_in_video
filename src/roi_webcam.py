51294bd0bb46b473f19e5379d1bdf14cca148669
archangel7431 <aljobith@gmail.com>
2023-06-02 14:28:28 +0530

    created roi_webcam

---
+++ src/roi_webcam.py
@@ -0,0 +1,48 @@
+import cv2
+
+# Callback function for mouse events
+def select_roi(event, x, y, flags, param):
+    global roi_top_left, roi_bottom_right, roi_selected
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        roi_top_left = (x, y)
+        roi_selected = False
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        roi_bottom_right = (x, y)
+        roi_selected = True
+
+# Initialize variables
+roi_top_left = None
+roi_bottom_right = None
+roi_selected = False
+
+# Start capturing video from the webcam
+video_capture = cv2.VideoCapture(0)  # Replace 0 with the index of your webcam if you have multiple cameras
+
+# Create a window and bind the mouse callback function
+cv2.namedWindow("Select ROI")
+cv2.setMouseCallback("Select ROI", select_roi)
+
+while True:
+    # Read the current frame from the video capture
+    ret, frame = video_capture.read()
+
+    # Display the frame
+    cv2.imshow("Select ROI", frame)
+
+    # Break the loop if ROI has been selected
+    if roi_selected:
+        break
+
+    # Wait for the 'q' key to exit the loop
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the video capture and close all windows
+video_capture.release()
+cv2.destroyAllWindows()
+
+# Print the selected ROI coordinates
+print("ROI Top Left: ", roi_top_left)
+print("ROI Bottom Right: ", roi_bottom_right)
