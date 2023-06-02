5875b45b963d25985bcf409ba2cf8d3dc368cf08
Riyawilson2000 <riyawilson16@gmail.com>
2023-06-02 13:02:36 +0530

    modified to get motion detection in webcams

--- src/gaussian_blur.py
+++ src/gaussian_blur.py
@@ -41,5 +41,40 @@ def gaussian_blur(path:str):
     cv2.destroyAllWindows()
 
     return blurred_frame
-path='./src/res/video_1.mp4'  
-blurred_frame =gaussian_blur(path)
+
+# first frame blurred
+def first_frame(webcam = False):
+
+    if webcam:
+        video = cv2.VideoCapture(0)
+        name = "./src/webcam/gauusain_blur/frame0.jpg"
+
+    else:
+        path = input("Enter the video file path: ")
+        video = cv2.VideoCapture(path)
+        name = "./src/res/video_1/gaussian_blur/frame0.jpg"
+
+    # Gaussian blur kernel size
+    kernel_size = (101, 51)  # Adjust the kernel size as needed
+
+    # Reading from frame
+    ret, frame = video.read()
+
+    # For the first frame
+    if ret:        
+        # convert frame to grayscale
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Apply Gaussian blur to smoothen the frame
+        blurred_frame = cv2.GaussianBlur(gray_frame, kernel_size, 0)
+
+        # Saving the extracted and smoothened grayscale image
+        cv2.imwrite(name, blurred_frame)
+        print(f"creating {name}") 
+  
+
+
+if __name__ == "__main__":
+    path='./src/res/video_1.mp4' 
+    blurred_frame =first_frame(path)
+    print(blurred_frame)
