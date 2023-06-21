d65f58cc709265e3db861e86210d99de700b5e0d
archangel7431 <aljobith@gmail.com>
2023-06-21 14:52:53 +0530

    updating flask server

--- src/app.py
+++ src/app.py
@@ -9,24 +9,26 @@ CORS(app)
 def home():
     return render_template("websitenew.html")
 
-@app.route('/data', methods=['GET'])
-def get_data():
-    # Process the request and generate a response
-    data = {'message': 'Hello from Flask server!'}
-    return jsonify(data)
 
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # Check if a file was uploaded
-        if 'file' in request.files:
-            file = request.files['file']
-            # Process the file as needed
-            # Example: Save the file to the current directory
-            file.save(file.filename)
-            return 'File uploaded successfully.'
-    # If no file was uploaded or GET request, render the HTML form
-    return render_template('websitenew.html')
+# @app.route('/data', methods=['GET'])
+# def get_data():
+#     # Process the request and generate a response
+#     data = {'message': 'Hello from Flask server!'}
+#     return jsonify(data)
+
+
+# @app.route('/upload', methods=['GET', 'POST'])
+# def upload_file():
+#     if request.method == 'POST':
+#         # Check if a file was uploaded
+#         if 'file' in request.files:
+#             file = request.files['file']
+#             # Process the file as needed
+#             # Example: Save the file to the current directory
+#             file.save(file.filename)
+#             return 'File uploaded successfully.'
+#     # If no file was uploaded or GET request, render the HTML form
+#     return render_template('websitenew.html')
 
 
 if __name__ == "__main__":
