a5578ced4c8ea3369f09c716f72bdd9bead63d9b
archangel7431 <aljobith@gmail.com>
2023-08-28 13:55:13 +0530

    connected 'upload device' to backend and sent file succesfully to server(backend)

--- src/app.py
+++ src/app.py
@@ -1,42 +1,24 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
-import logging
+
+import os
 
 app = Flask(__name__)
 CORS(app)
-
-logging.basicConfig(filename="server.log", level=logging.DEBUG)
-
 
 @app.route("/")
 def home():
     return render_template("websitenew.html")
 
-
-@app.route('/data', methods=['GET'])
-def get_data():
-    # Process the request and generate a response
-    data = {'message': 'Hello from Flask server!'}
-    return jsonify(data)
-
-
-@app.route('/upload', methods=['POST'])
+@app.route("/upload", methods=["POST"])
 def upload_file():
-    # Accessing all headers
-    logging.debug('Request Headers:')
-    for header, value in request.headers.items():
-        logging.debug(f"{header}: {value}")
-
-    logging.debug('Request JSON Payload:')
-    logging.debug(request.get_json())
-
-
-    uploaded_file = request.files['data']
-    uploaded_file.save(uploaded_file.filename)  # Save the file to disk
-    # Process the uploaded file here
-    # Add your file processing logic
-
-    return 'Upload endpoint'
+    file = request.files["file"]
+    if file:
+        filename = file.filename
+        file.save(os.path.join("uploads", filename))
+        return jsonify({"message": "file uploaded successfully"}), 200
 
 if __name__ == "__main__":
-    app.run()
+    if not os.path.exists("uploads"):
+        os.makedirs("uploads")
+    app.run("localhost", debug=True)
